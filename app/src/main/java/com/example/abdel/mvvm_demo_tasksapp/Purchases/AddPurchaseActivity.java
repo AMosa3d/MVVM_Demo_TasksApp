5d9c4c4a8c72e1057cc6ae13e568e0d1dc16c924
AMosa3d <abdelrahman.mosa3d.fcis@hotmail.com>
2019-03-02 01:27:54 +0200

    add parts of purchase logic and UI

---
+++ app/src/main/java/com/example/abdel/mvvm_demo_tasksapp/Purchases/AddPurchaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.abdel.mvvm_demo_tasksapp.Purchases;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPurchaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
